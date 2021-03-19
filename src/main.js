b894a2d4385baae1e791de1daf5b2482c83a1954
João Salomão <joaosalomao.613@gmail.com>
2021-03-19 04:57:55 -0300

    Adding lodash and vue-multselect

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import axios from 'axios'
 import App from './App.vue'
 import router from './router'
+import Multiselect from 'vue-multiselect'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import 'vue-multiselect/dist/vue-multiselect.min.css';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
@@ -12,6 +14,7 @@ Vue.config.productionTip = false;
 Vue.prototype.$http = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
+Vue.component('multiselect', Multiselect);
 
 new Vue({
   router,
