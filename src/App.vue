279fd5ef63442878d39d6cb0cf246ef89e37364d
João Salomão <joaosalomao.613@gmail.com>
2021-03-19 01:28:56 -0300

    Changing buttons default border-radius

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,18 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
   padding: 30px;
+  background-color: rgba(128, 128, 128, 0.4);
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.btn,
+.btn-sm,
+.btn-group-sm > .btn {
+  border-radius: 0;
 }
 </style>
