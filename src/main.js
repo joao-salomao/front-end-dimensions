2890d425c1652f37c82b1c9eadff9bf5201d4cc4
João Salomão <joaosalomao.613@gmail.com>
2021-03-19 01:46:04 -0300

    Adding vue-resource plugin

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueResource from 'vue-resource'
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueResource);
 Vue.config.productionTip = false
 
 new Vue({
