151c84626be7db202f6921fc20fc348a3feea32a
João Salomão <joaosalomao.613@gmail.com>
2021-03-19 21:35:52 -0300

    Componentizing form

---
+++ src/views/Dimension/Form.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-form
+    @submit.prevent="onSubmit"
+    class="p-4"
+    style="background-color: white; height: 80%"
+  >
+    <b-form-group label="Nome da dimensão">
+      <b-form-input size="sm" v-model="title" required trim />
+    </b-form-group>
+    <b-button size="sm" type="submit" class="mr-2" variant="success">
+      <b-spinner small type="grow" v-if="isSubmitting" />
+      <span v-else> Salvar </span>
+    </b-button>
+    <b-button size="sm" variant="light" @click="$router.push('/dimension')"
+      >Cancelar</b-button
+    >
+  </b-form>
+</template>
+<script>
+export default {
+  props: {
+    dimension: {
+      type: Object,
+      default: null,
+    },
+    isSubmitting: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  mounted() {
+    if (this.dimension) {
+      this.title = this.dimension.title;
+    }
+  },
+  data() {
+    return {
+      title: null,
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("submit", { title: this.title });
+    },
+  },
+};
+</script>
